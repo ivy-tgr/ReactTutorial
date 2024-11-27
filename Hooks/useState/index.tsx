4bc9b92c7219e356f2e791a27fd6199005f07798
ivy-tgr <tobias.gretler@axonivy.com>
2024-11-27 16:35:57 +0100

    starting with hooks

---
+++ Hooks/useState/index.tsx
@@ -0,0 +1,36 @@
+import { createRoot } from 'react-dom/client'
+
+let state: any
+let setState: any
+
+export function useState<State>(initialState: State) {
+	if (state === undefined) {
+		state = initialState
+		setState = (newState: State) => {
+			state = newState
+			render()
+		}
+	} 
+	return [state, setState] as [State, (newState: State) => void]
+}
+
+function Counter() {
+	const [count, setCount] = useState(0)
+	const increment = () => setCount(count + 1)
+
+	return (
+		<div className="counter">
+			<button onClick={increment}>{count}</button>
+		</div>
+	)
+}
+
+const rootEl = document.createElement('div')
+document.body.append(rootEl)
+const appRoot = createRoot(rootEl)
+
+function render() {
+	appRoot.render(<Counter />)
+}
+
+render()
